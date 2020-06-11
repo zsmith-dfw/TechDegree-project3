4f7a64264e6cd39335b80c0c4330286de46b549c
Zach Smith <rysskismith@gmail.com>
2020-06-11 14:04:49 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 function setFocus (){
     document.getElementById("name").focus();
 }
@@ -12,15 +13,53 @@ const changeColor = document.querySelector('label[for="color"]');
 changeColor.textContent = 'Please select a T-shirt theme.'
 const hideColors = document.getElementById('color');
 hideColors.hidden=true;
-console.log(hideColors);
 
+
+const punsTheme = document.querySelectorAll('#design option')[1]
+const loveTheme = document.querySelectorAll('#design option')[2]
 const showDesign = document.querySelector('#design')
-const heartOption = document.querySelector('option[value="heart jscolors-js-puns]');
-const punOption = document.querySelector('option[value="js puns"]');
-showDesign.addEventListener('change', (event) => {
-  if (punOption.selected = true) {
-      heartOption.hidden = true
 
-  }
+
+
+showDesign.addEventListener('change', (e) => {
+
+  if(punsTheme.selected = true) {
+    hideColors.hidden=false;
+
+    const color1 = document.querySelectorAll('#color option')[3]
+    const color2 = document.querySelectorAll('#color option')[4]
+    const color3 = document.querySelectorAll('#color option')[5]
+    const color4 = document.querySelectorAll('#color option')[0]
+    const color5 = document.querySelectorAll('#color option')[1]
+    const color6 = document.querySelectorAll('#color option')[2]
+
+    color1.hidden = true
+    color2.hidden = true
+    color3.hidden = true 
+    color4.hidden = false
+    color5.hidden = false
+    color6.hidden = false
+
+} else if (loveTheme.selected = true) {
+  hideColors.hidden = false;
+  color1.hidden = false
+  color2.hidden = false
+  color3.hidden = false
+  color4.hidden = true
+  color5.hidden = true
+  color6.hidden = true
+
 }
+
+});
+
+
+// IF design option === 'Theme - JS Puns'
+// show color drop down
+// hide/disable colors from the other theme
+// show colors from the correct theme (so it will always unhide if you switch back and forth)
+// ELSE IF design option === 'Theme - I :heart: JS'
+// show color drop down
+// hide/disable colors from the other theme
+// show colors from the correct theme
 
